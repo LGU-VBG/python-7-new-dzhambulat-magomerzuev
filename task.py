04cf9bc60fe35d4d4fadb4292bbe81d72d68d17a
dzambulat-magomerzuev <pravdiviy01@gmail.com>
2025-03-05 13:54:34 +0300

    доделать

--- task.py
+++ task.py
@@ -1,1 +1,18 @@
+def merge_lists_to_dict(keys, values):
+    result_dict = dict(zip(key, val))
+    return result_dict
 
+
+key = ['a', 'b', 'c']
+val = [1, 2, 3]
+
+
+merg = merge_lists_to_dict(key, val)
+
+
+print(merg)
+# Вызов функции с использованием аргументов с ключевыми словами
+result = merge_lists_to_dict(list1=[1, 2, 3], list2=[4, 5, 6])
+
+# Второй вызов функции с одним позиционным аргументом и одним аргументом с ключевым словом
+result2 = merge_lists_to_dict([1, 2, 3], list2=[4, 5, 6])
